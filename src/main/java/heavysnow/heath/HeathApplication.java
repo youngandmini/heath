2dd61301edc528c49b92cac367fadc58b2ae7aeb
youngmin <dudals9901@naver.com>
2023-11-01 12:03:44 +0900

    Initial commit

---
+++ src/main/java/heavysnow/heath/HeathApplication.java
@@ -0,0 +1,13 @@
+package heavysnow.heath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeathApplication.class, args);
+	}
+
+}
