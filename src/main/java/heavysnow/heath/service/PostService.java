5a83701ccd441c627477adbe148bc2171bbdd93f
youngmin <dudals9901@naver.com>
2023-11-03 17:58:40 +0900

    fix: getPostListByMember의 parameter 수정

--- src/main/java/heavysnow/heath/service/PostService.java
+++ src/main/java/heavysnow/heath/service/PostService.java
@@ -18,7 +18,15 @@ public class PostService {
 
     private final PostRepository postRepository;
 
-    public PostListResponseDto getPostListByMember(Long memberId, Pageable pageable) {
+
+    /**
+     * 마이페이지에 사용되는 게시글 리스트를 가져오는 메서드
+     * @param memberId: 회원 아이디
+     * @param page: 페이지 넘버
+     * @return PostListResponseDto
+     */
+    public PostListResponseDto getPostListByMember(Long memberId, int page) {
+        Pageable pageable = PageRequest.of(page, 9);
         Page<Post> postPage = postRepository.findPageByMember(memberId, pageable);
         return PostListResponseDto.of(postPage);
     }
