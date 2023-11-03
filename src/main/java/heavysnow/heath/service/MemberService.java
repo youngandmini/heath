0fc8ed3f0bcd180aee3b7dfe08e6f3229868411a
GwanHwi1 <GwanHwi1@github.com>
2023-11-03 15:56:04 +0900

    feat: 회원정보 저장, 수정

---
+++ src/main/java/heavysnow/heath/service/MemberService.java
@@ -0,0 +1,37 @@
+package heavysnow.heath.service;
+
+import heavysnow.heath.domain.Member;
+import heavysnow.heath.dto.MemberDto;
+import heavysnow.heath.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member createUser(MemberDto dto){
+        Member encoding = Member.builder()
+                .username(dto.getUsername())
+                .nickname(dto.getNickname())
+                .userStatusMessage(dto.getUserStatusMessage())
+                .profileImgPath(dto.getProfileImgPath())
+                .build();
+        return memberRepository.save(encoding);
+    }
+
+    @Transactional
+    public Member EditMember(String userName, String nickName, String userStatusMessage, String profileImgPath) {
+        Member entity = memberRepository.findByUsername(userName).orElse(null);
+        if (entity == null){
+            System.out.println("회원정보가 없습니다.");
+        }
+        entity.update(nickName, userStatusMessage, profileImgPath);
+        return memberRepository.findByUsername("userName").orElse(null);
+    }
+}
---
+++ src/test/java/heavysnow/heath/service/MemberServiceTest.java
@@ -0,0 +1,65 @@
+package heavysnow.heath.service;
+
+import heavysnow.heath.domain.Member;
+import heavysnow.heath.dto.MemberDto;
+import heavysnow.heath.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    void createUser() {
+        //given
+        MemberDto memberDto = MemberDto.builder()
+                .username("aaa")
+                .nickname("gwan")
+                .userStatusMessage("fighting!!!")
+                .profileImgPath("None")
+                .build();
+
+        //when
+        Member saveId = memberService.createUser(memberDto);
+
+        //then
+        Member findMember = memberRepository.findByUsername(memberDto.getUsername()).orElse(null);
+        assertNotNull(findMember);
+        assertEquals(memberDto.getUsername(), findMember.getUsername());
+        assertEquals(memberDto.getNickname(), findMember.getNickname());
+        assertEquals(memberDto.getUserStatusMessage(), findMember.getUserStatusMessage());
+        assertEquals(memberDto.getProfileImgPath(), findMember.getProfileImgPath());
+    }
+
+    @Test
+    void EditMember() {
+        MemberDto memberDto = MemberDto.builder()
+                .username("aaa")
+                .nickname("gwan")
+                .userStatusMessage("fighting!!!")
+                .profileImgPath("None")
+                .build();
+        memberService.createUser(memberDto);
+        memberService.EditMember("aaa", "hwi", "good!", "Null");
+        Member result = memberRepository.findByUsername("aaa").orElse(null);
+        assertEquals(result.getUsername(), "aaa");
+        assertEquals(result.getNickname(), "hwi");
+        assertEquals(result.getUserStatusMessage(), "good!");
+        assertEquals(result.getProfileImgPath(), "Null");
+
+    }
+
+}
