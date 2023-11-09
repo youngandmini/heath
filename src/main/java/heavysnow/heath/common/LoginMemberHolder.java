8556f0f3fd35a452b7c359059fa6ebbfcd4af393
youngmin <dudals9901@naver.com>
2023-11-09 15:03:58 +0900

    feat: 로그인 기능 구현 - ExpiringMap 활용, 컨트롤러에서 LoginMemberHolder에 접근하여 로그인한 멤버 식별

---
+++ src/main/java/heavysnow/heath/common/LoginMemberHolder.java
@@ -0,0 +1,39 @@
+package heavysnow.heath.common;
+
+import net.jodah.expiringmap.ExpirationPolicy;
+import net.jodah.expiringmap.ExpiringMap;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+
+@Component
+public class LoginMemberHolder {
+
+    /**
+     * 현재 로그인한 멤버의 정보를 토큰 - memberId 형태로 담음
+     * 사용하지 않은지 10분이 지나면 자동으로 만료되도록 설정
+     */
+    private static final ExpiringMap<String, Long> loginMembers = ExpiringMap.builder()
+            .maxSize(500)
+            .expirationPolicy(ExpirationPolicy.ACCESSED)
+            .expiration(30, TimeUnit.MINUTES)
+            .build();
+
+    /**
+     * 헤더의 토큰 정보를 가지고 로그인한 멤버의 id를 찾음
+     * @param token
+     * @return
+     */
+    public static Long findLoginMemberId(String token) {
+        return loginMembers.get(token);
+    }
+
+    public void login(String token, Long memberId) {
+        loginMembers.put(token, memberId);
+    }
+
+    public void logout(String token) {
+        loginMembers.remove(token);
+    }
+}
---
+++ src/main/java/heavysnow/heath/service/LoginService.java
@@ -0,0 +1,89 @@
+package heavysnow.heath.service;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import heavysnow.heath.common.LoginMemberHolder;
+import heavysnow.heath.domain.Member;
+import heavysnow.heath.dto.MemberDto;
+import heavysnow.heath.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import net.jodah.expiringmap.ExpirationPolicy;
+import net.jodah.expiringmap.ExpiringMap;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final Decoder decoder = Base64.getDecoder();
+
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+    private final LoginMemberHolder loginMemberHolder;
+
+    /**
+     * token을 받아서 로그아웃 수행
+     * @param token
+     */
+    public void logout(String token) {
+        loginMemberHolder.logout(token);
+    }
+
+    /**
+     * 로그인 요청이 들어왔을때, 토큰을 파싱하여 회원을 식별한다음,
+     * 이미 존재한 회원이라면 로그인하고
+     * 처음 방문한 회원이라면 로그인 정보를 저장한 다음 로그인을 수행
+     * @param token
+     */
+    public void login(String token) {
+        Map<String, String> map = decodeToken(token);
+
+        String email = map.get("email");
+        String nickname = map.get("name");
+        String profileImage = map.get("image");
+
+        Optional<Member> findMember = memberRepository.findByUsername(email);
+        if (findMember.isPresent()) {
+            doLogin(token, findMember.get().getId());
+        } else {
+            //처음 방문한 회원이라면, 회원정보를 DB에 저장 후 로그인
+            Long memberId = joinMember(email, nickname, profileImage);
+            doLogin(token, memberId);
+        }
+    }
+
+    //로그인 수행
+    private void doLogin(String token, Long memberId) {
+        loginMemberHolder.login(token, memberId);
+    }
+
+    //회원 가입
+    private Long joinMember(String email, String nickname, String profileImage) {
+        MemberDto memberDto = new MemberDto(email, nickname, null, profileImage);
+        return memberService.createUser(memberDto);
+    }
+
+    private Map<String, String> decodeToken(String token) {
+        String[] chunks = token.split("\\.");
+        String payload = new String(decoder.decode(chunks[1]));
+
+        Map<String, String> map = new HashMap<>();
+        try {
+            map = mapper.readValue(payload, Map.class);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        return map;
+    }
+}
