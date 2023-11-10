27503ecc5e1ef53aa4011f57214062644c92bbd7
GwanHwi1 <GwanHwi1@github.com>
2023-11-10 18:25:05 +0900

    feat: goal목표 추가 기능

--- src/main/java/heavysnow/heath/controller/MemberController.java
+++ src/main/java/heavysnow/heath/controller/MemberController.java
@@ -1,26 +1,33 @@
 package heavysnow.heath.controller;
 
+import heavysnow.heath.dto.GoalCreationDto;
 import heavysnow.heath.dto.MemberDto;
 import heavysnow.heath.repository.MemberRepository;
+import heavysnow.heath.service.GoalService;
 import heavysnow.heath.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
 
+    private final GoalService goalService;
+
     private final MemberService memberService;
 
     private final MemberRepository memberRepository;
 
-    @PostMapping("/user/{userId}")
+    @PatchMapping("/user/{userId}")
     public ResponseEntity<String> updateMember(@PathVariable Long userId, @RequestBody MemberDto memberDto) {
         memberService.editMember(userId, memberDto);
         return ResponseEntity.ok("Member updated successfully");
+    }
+
+    @PostMapping("members/{memberId}/goals")
+    public ResponseEntity<Long> addGoal(@PathVariable Long memberId, @RequestBody GoalCreationDto goalCreationDto) {
+        Long goalId = goalService.createGoalForMember(memberId, goalCreationDto);
+        return ResponseEntity.ok(goalId);
     }
 }
