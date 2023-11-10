ee847742267c0af4628928791b4657150c8eb3ef
youngmin <dudals9901@naver.com>
2023-11-10 14:36:41 +0900

    feat: PostController 생성 - 조회 요청

---
+++ src/main/java/heavysnow/heath/controller/PostController.java
@@ -0,0 +1,36 @@
+package heavysnow.heath.controller;
+
+import heavysnow.heath.common.LoginMemberHolder;
+import heavysnow.heath.dto.postdto.PostDetailResponseDto;
+import heavysnow.heath.exception.UnauthorizedException;
+import heavysnow.heath.service.CommentService;
+import heavysnow.heath.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+    private final CommentService commentService;
+
+    /**
+     * 게시글 상세 조회 요청
+     */
+    @GetMapping("/posts/{postId}")
+    @ResponseStatus(HttpStatus.OK)
+    public PostDetailResponseDto detailedPost(@PathVariable("postId") Long postId, HttpServletRequest request) {
+        Optional<Long> loginMemberIdOptional = LoginMemberHolder.findLoginMemberId(request.getHeader("accessToken"));
+        Long loginMemberId = loginMemberIdOptional.orElse(null);
+
+        return postService.getPostWithDetail(postId, loginMemberId);
+    }
+
+
+}
