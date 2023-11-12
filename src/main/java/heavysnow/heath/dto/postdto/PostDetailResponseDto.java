ee8334f3ca3fd1ca221349a02734366e63711940
youngmin <dudals9901@naver.com>
2023-11-12 17:04:16 +0900

    fix: Post 관련 dto의 post image 관련 필드를 List<String> postImgUrls로 통일

--- src/main/java/heavysnow/heath/dto/postdto/PostDetailResponseDto.java
+++ src/main/java/heavysnow/heath/dto/postdto/PostDetailResponseDto.java
@@ -2,6 +2,7 @@ package heavysnow.heath.dto.postdto;
 
 import heavysnow.heath.domain.Comment;
 import heavysnow.heath.domain.Post;
+import heavysnow.heath.domain.PostImage;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -26,7 +28,7 @@ public class PostDetailResponseDto {
     private int consecutiveDays;
     private int liked;
     private boolean isLiked;
-    private List<PostImageInfo> imgs;
+    private List<String> postImgUrls;
     private List<CommentResponseDto> comments;
 
     public boolean isLiked() {
@@ -45,7 +47,8 @@ public class PostDetailResponseDto {
                 post.getConsecutiveDays(),
                 post.getMemberPostLikedList().size(),
                 isMemberPostLiked(post, memberId),
-                PostImageInfo.listOf(post.getPostImages()),
+                post.getPostImages().stream().map(PostImage::getImgUrl).collect(Collectors.toList()),
+//                PostImageInfo.listOf(post.getPostImages()),
                 CommentResponseDto.listOf(parentComments)
         );
     }
