e66102e7ea9656da4d0c92cbc137071a77eada12
jungbin97 <ljb5054@gmail.com>
2023-11-13 12:59:33 +0900

    fix: 병합 충돌 해결

--- src/main/java/heavysnow/heath/controller/LoginController.java
+++ src/main/java/heavysnow/heath/controller/LoginController.java
@@ -1,7 +1,7 @@
 package heavysnow.heath.controller;
 
 
-import heavysnow.heath.common.LoginMemberHolder;
+import heavysnow.heath.dto.LoginResponseDto;
 import heavysnow.heath.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
