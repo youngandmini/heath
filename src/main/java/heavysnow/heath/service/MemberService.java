9051610bc2614b26f9e2298bef376fa71e3d93b2
youngmin <dudals9901@naver.com>
2023-11-13 22:07:21 +0900

    refactor: MemberService 주석 수정

--- src/main/java/heavysnow/heath/service/MemberService.java
+++ src/main/java/heavysnow/heath/service/MemberService.java
@@ -17,23 +17,40 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+
+    /**
+     * 새로운 회원을 생성하는 메서드
+     * @param memberDto: 이 정보를 이용해 회원가입
+     * @return: 회원의 id를 반환
+     */
     @Transactional
-    public Long createUser(MemberRequest dto){
-        Member savedMember = memberRepository.save(dto.toEntity());
+    public Long createUser(MemberRequest memberDto){
+        Member savedMember = memberRepository.save(memberDto.toEntity());
         return savedMember.getId();
     }
 
+    /**
+     * 회원 정보를 수정하는 메서드
+     * @param loginMemberId: 해당 멤버로 접속한 사용자가
+     * @param memberId: 해당 멤버에
+     * @param memberDto: 이 정보를 이용해 수정 요청
+     */
     @Transactional
-    public void editMember(Long loginMemberId, Long memberId, MemberRequest dto) {
+    public void editMember(Long loginMemberId, Long memberId, MemberRequest memberDto) {
         Member member = memberRepository.findById(memberId).orElseThrow(NotFoundException::new);
 
         if (!loginMemberId.equals(memberId)) {
             throw new ForbiddenException();
         }
 
-        member.update(dto.getNickname(), dto.getUserStatusMessage(), dto.getProfileImgUrl());
+        member.update(memberDto.getNickname(), memberDto.getUserStatusMessage(), memberDto.getProfileImgUrl());
     }
 
+    /**
+     * 회원을 탈퇴하는 메서드
+     * @param memberId: 해당 멤버에 대해
+     * @param LoginMemberId: 해당 접속한 멤버가 탍퇴 요청
+     */
     @Transactional
     public void deleteMember(Long memberId, Long LoginMemberId) {
         Member findMember = memberRepository.findById(memberId).orElseThrow(NotFoundException::new);
@@ -46,6 +63,12 @@ public class MemberService {
 
     //fetch, join
     //Member를 반환하는 것에서 MemberResponseDto를 반환하도록 변경
+
+    /**
+     * 해당 회원의 정보를 목표와 함께 조회하는 메서드
+     * @param memberId: 해당 멤버의 정보를 요청
+     * @return: 멤버 + 목표 정보
+     */
     public MemberResponse findMemberWithGoals(Long memberId){
         Member member = memberRepository.findByIdWithGoals(memberId).orElseThrow(NotFoundException::new);
 
