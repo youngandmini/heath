63940f843e32e467b9be5c5c33b535ce3789dc88
youngmin <dudals9901@naver.com>
2023-11-13 22:22:58 +0900

    refactor: MemberPostLikedRepository 주석 수정

--- src/main/java/heavysnow/heath/repository/MemberPostLikedRepository.java
+++ src/main/java/heavysnow/heath/repository/MemberPostLikedRepository.java
@@ -1,16 +1,19 @@
 package heavysnow.heath.repository;
 
-import heavysnow.heath.domain.Member;
 import heavysnow.heath.domain.MemberPostLiked;
 import heavysnow.heath.domain.MemberPostLikedPK;
-import heavysnow.heath.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberPostLikedRepository extends JpaRepository<MemberPostLiked, MemberPostLikedPK> {
 
+    /**
+     * 해당 멤버가 해당 게시글에 좋아요를 눌렀는지를 확인
+     * @param memberId: 해당 멤버가
+     * @param postId: 해당 게시글에 좋아요를 눌렀는지 확인
+     * @return: 좋아요 레코드를 반환
+     */
     Optional<MemberPostLiked> findByMemberIdAndPostId(@Param("memberId") Long memberId, @Param("postId") Long postId);
 }
