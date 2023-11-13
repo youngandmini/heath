bc7a9ab99e1bd8f7fa7f7335882db247fd173222
youngmin <dudals9901@naver.com>
2023-11-13 21:56:30 +0900

    refactor: CommentService 주석 수정

--- src/main/java/heavysnow/heath/service/CommentService.java
+++ src/main/java/heavysnow/heath/service/CommentService.java
@@ -25,9 +25,12 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     /**
-     * 생성 : 맴버의 게시글에 대한 댓글 생성하는 메서드
-     * @param commentCreateRequest
-     * @return
+     * 게시글에 대한 댓글을 생성하는 메서드
+     * @param postId: 해당 게시글에
+     * @param commentCreateRequest: 이 내용으로
+     * @param parentCommentId: 이 댓글 하위에
+     * @param loginMemberId: 이 멤버가 댓글을 등록한다.
+     * @return: 댓글의 id를 반환
      */
     public CommentCreateResponse createComment(Long postId, CommentCreateRequest commentCreateRequest, Long parentCommentId, Long loginMemberId) {
         Post post = postRepository.findById(postId).orElseThrow(NotFoundException::new);
@@ -45,6 +48,13 @@ public class CommentService {
         return CommentCreateResponse.of(savedcomment.getId());
     }
 
+    /**
+     * 게시글에 대한 댓글을 수정하는 메서드
+     * @param postId: 해당 게시글에
+     * @param commentId: 해당 댓글을
+     * @param commentUpdateRequest: 이 내용으로
+     * @param loginMemberId: 이 멤버가 댓글을 수정한다.
+     */
     public void updateComment(Long postId, Long commentId, CommentUpdateRequest commentUpdateRequest, Long loginMemberId) {
         // 댓글 존재 확인
         Comment comment = commentRepository.findById(commentId).orElseThrow(NotFoundException::new);
@@ -61,10 +71,15 @@ public class CommentService {
         comment.updateComment(commentUpdateRequest.getContent());
     }
 
-    public void deleteComment(Long commentId, Long memberId) {
+    /**
+     * 댓글을 삭제하는 메서드
+     * @param commentId: 해당 댓글에 대해
+     * @param loginMemberId: 해당 멤버가 댓글을 삭제한다.
+     */
+    public void deleteComment(Long commentId, Long loginMemberId) {
         Comment comment = commentRepository.findById(commentId).orElseThrow(NotFoundException::new);
 
-        if (!comment.getMember().getId().equals(memberId)) {
+        if (!comment.getMember().getId().equals(loginMemberId)) {
             throw new ForbiddenException();
         }
         commentRepository.delete(comment);
