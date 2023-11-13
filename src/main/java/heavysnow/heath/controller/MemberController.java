4129796fec932823f9880e83cd98ebb52f773707
youngmin <dudals9901@naver.com>
2023-11-13 18:13:34 +0900

    refactor: 주석을 수정하고 @ResponseStatus를 이용해 Http 상태 코드를 정의

--- src/main/java/heavysnow/heath/controller/MemberController.java
+++ src/main/java/heavysnow/heath/controller/MemberController.java
@@ -9,6 +9,7 @@ import heavysnow.heath.service.MemberService;
 import heavysnow.heath.service.PostService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,116 +21,130 @@ import java.util.Optional;
 public class MemberController {
 
     private final GoalService goalService;
-
     private final MemberService memberService;
-
     private final PostService postService;
 
     /**
-     * 마이페이지 회원 정보 + 목표가져오는 메서드
-     *
-     * @param memberId 조회할 멤버 아이디
-     * @return ResponseEntity
+     * 마이페이지에서 회원 정보와 목표를 가져오기 위한 요청
+     * @param memberId: 조회할 멤버 아이디
+     * @return: 회원 정보와 목표들을 반환
      */
     @GetMapping("/{memberId}")
-    public ResponseEntity<MemberResponseDto> getMember(@PathVariable("memberId") Long memberId) {
+    @ResponseStatus(HttpStatus.OK)
+    public MemberResponseDto getMember(@PathVariable("memberId") Long memberId) {
         MemberResponseDto response = memberService.findMemberWithGoals(memberId);
-        return ResponseEntity.ok(response);
+        return response;
     }
 
 
     /**
-     * 마이페이지 해당 년도 해당 월의 운동 정보 가져오는 메서드
-     *
-     * @param memberId 조회할 멤버 아이디
-     * @param year     조회할 년도
-     * @param month    조회할 월
-     * @return ResponseEntity<PostDatesResponseDto>
+     * 마이페이지에서 해당 년도 해당 월의 운동 정보 가져오기 위한 요청
+     * @param memberId: 조회할 멤버 아이디
+     * @param year: 조회할 년도
+     * @param month: 조회할 월
+     * @return: 해당 년도 해당 월의 운동 정보를 리스트 형태로 반환
      */
     @GetMapping("/{memberId}/dates")
-    public ResponseEntity<PostDatesResponseDto> getExerciseDates(@PathVariable("memberId") Long memberId, @RequestParam("year") int year, @RequestParam("month") int month) {
+    @ResponseStatus(HttpStatus.OK)
+    public PostDatesResponseDto getExerciseDates(@PathVariable("memberId") Long memberId, @RequestParam("year") int year, @RequestParam("month") int month) {
         PostDatesResponseDto response = postService.getPostDates(memberId, year, month);
-        return ResponseEntity.ok(response); // 200 OK
+        return response;
     }
 
     /**
-     * 회원 운동 인증글(포스트) 가져오기(9개씩 불러온다.)
-     *
-     * @param memberId 조회할 멤버 아이디
-     * @param page     게시글 수
-     * @return ResponseEntity<PostListResponseDto>
+     * 마이페이지에서 회원 운동 인증글(포스트) 가져오기 위한 요청
+     * @param memberId: 조회할 멤버 아이디
+     * @param page: 페이지 번호
+     * @return: 인증글을 9개씩 페이징하여 반환
      */
     @GetMapping("/{memberId}/posts")
-    public ResponseEntity<PostListResponseDto> getMemberPosts(@PathVariable("memberId") Long memberId, @RequestParam(value = "page", defaultValue = "0") int page) {
+    @ResponseStatus(HttpStatus.OK)
+    public PostListResponseDto getMemberPosts(@PathVariable("memberId") Long memberId, @RequestParam(value = "page", defaultValue = "0") int page) {
         PostListResponseDto response = postService.getPostListByMember(memberId, page);
-        return ResponseEntity.ok(response);
+        return response;
     }
 
 
     /**
-     * 회원 탈퇴
-     * @param memberId 삭제할 멤버 아이디
-     * @param request 로그인 정보
-     * @return ResponseEntity<Void>
+     * 특정 회원을 탈퇴시키기 위한 요청
+     * @param memberId: 삭제할 멤버 아이디
+     * @param request: 로그인 정보
      */
     @DeleteMapping("/{memberId}")
-    public ResponseEntity<Void> deleteMember(@PathVariable("memberId") Long memberId, HttpServletRequest request) {
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteMember(@PathVariable("memberId") Long memberId, HttpServletRequest request) {
         // 인증 토큰 확인
         Optional<Long> loginMemberOptional = LoginMemberHolder.findLoginMemberId(request.getHeader("accessToken"));
         Long loginMemberId = loginMemberOptional.orElseThrow(UnauthorizedException::new);
 
         // 회원 탈퇴
         memberService.deleteMember(memberId, loginMemberId);
-
-        return ResponseEntity.ok().build();
     }
 
-
+    /**
+     * 특정 회원의 정보를 수정하기 위한 요청
+     * @param memberId: 수정할 회원 아이디
+     * @param memberDto: 수정할 회원 정보
+     * @param request: 로그인 정보
+     */
     @PatchMapping("/{memberId}")
-    public ResponseEntity<Void> updateMember(@PathVariable("memberId") Long memberId, @RequestBody MemberDto memberDto,
+    @ResponseStatus(HttpStatus.OK)
+    public void updateMember(@PathVariable("memberId") Long memberId, @RequestBody MemberDto memberDto,
                                              HttpServletRequest request) {
         Optional<Long> loginMemberIdOptional = LoginMemberHolder.findLoginMemberId(request.getHeader("accessToken"));
         Long loginMemberId = loginMemberIdOptional.orElseThrow(UnauthorizedException::new);
 
         memberService.editMember(loginMemberId, memberId, memberDto);
-        return ResponseEntity.ok().build();
     }
 
+    /**
+     * 특정 회원의 새로운 목표를 생성하기 위한 요청
+     * @param memberId: 멤버 아이디
+     * @param goalCreationDto: 목표 내용
+     * @param request: 로그인 정보
+     * @return: 생성된 목표의 아이디
+     */
     @PostMapping("/{memberId}/goals")
-    public ResponseEntity<GoalIdResponseDto> addGoal(@PathVariable("memberId") Long memberId, @RequestBody GoalCreationDto goalCreationDto,
+    @ResponseStatus(HttpStatus.OK)
+    public GoalIdResponseDto addGoal(@PathVariable("memberId") Long memberId, @RequestBody GoalCreationDto goalCreationDto,
                                                      HttpServletRequest request) {
         Optional<Long> loginMemberIdOptional = LoginMemberHolder.findLoginMemberId(request.getHeader("accessToken"));
         Long loginMemberId = loginMemberIdOptional.orElseThrow(UnauthorizedException::new);
 
         GoalIdResponseDto response = goalService.createGoalForMember(loginMemberId, memberId, goalCreationDto);
-        return ResponseEntity.ok(response);
+        return response;
     }
 
     /**
-     * 회원 목표를 성공 / 미성공 체크
-     * @param memberId 수정할 멤버 아이디
-     * @param goalId 수정할 목표 아이디
-     * @param goalUpdateDto content, isAchieved
-     * @param request 로그인 정보
-     * @return ResponseEntity<Void>
+     * 회원 목표를 성공 / 미성공으로 변경하기 위한 요청
+     * @param memberId: 수정할 목표가 속한 멤버 아이디
+     * @param goalId: 수정할 목표 아이디
+     * @param goalUpdateDto: 달성 여부
+     * @param request: 로그인 정보
      */
     @PatchMapping("/{memberId}/goals/{goalId}")
-    public ResponseEntity<Void> updateGoal(@PathVariable("memberId") Long memberId, @PathVariable("goalId") Long goalId, @RequestBody GoalUpdateDto goalUpdateDto, HttpServletRequest request) {
+    @ResponseStatus(HttpStatus.OK)
+    public void updateGoal(@PathVariable("memberId") Long memberId, @PathVariable("goalId") Long goalId, @RequestBody GoalUpdateDto goalUpdateDto, HttpServletRequest request) {
         // 인증 토큰 확인
         Optional<Long> loginMemberOptional = LoginMemberHolder.findLoginMemberId(request.getHeader("accessToken"));
         Long loginMemberId = loginMemberOptional.orElseThrow(UnauthorizedException::new);
 
         goalService.updateGoalForMember(loginMemberId, memberId, goalId, goalUpdateDto);
-
-        return ResponseEntity.ok().build();
     }
 
+    /**
+     * 특정 회원의 특정 목표를 삭제하기 위한 요청
+     * @param memberId: 삭제할 목표가 속한 멤버 아이디
+     * @param goalId: 삭제할 목표 아이디
+     * @param request: 로그인 정보
+     */
     @DeleteMapping("/{memberId}/goals/{goalId}")
-    public ResponseEntity<Void> deleteGoal(@PathVariable("memberId") Long memberId, @PathVariable("goalId") Long goalId,
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteGoal(@PathVariable("memberId") Long memberId, @PathVariable("goalId") Long goalId,
                                            HttpServletRequest request) {
         Optional<Long> loginMemberIdOptional = LoginMemberHolder.findLoginMemberId(request.getHeader("accessToken"));
         Long loginMemberId = loginMemberIdOptional.orElseThrow(UnauthorizedException::new);
+
         goalService.deleteGoalForMember(loginMemberId, memberId, goalId);
-        return ResponseEntity.ok().build();
     }
 }
