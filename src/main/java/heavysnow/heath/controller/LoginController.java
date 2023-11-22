b636ff78d99e5d54fe9d0f4bf3dc69af713304b7
youngmin <dudals9901@naver.com>
2023-11-22 11:53:11 +0900

    refactor: 로그인 컨트롤러 로직에 로그 추가

--- src/main/java/heavysnow/heath/controller/LoginController.java
+++ src/main/java/heavysnow/heath/controller/LoginController.java
@@ -23,24 +23,26 @@ public class LoginController {
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
     public LoginResponse login(HttpServletRequest request, HttpServletResponse response) {
-        log.info("새로운 로그인 요청");
+        log.info("새로운 로그인 요청 발생");
         String token = request.getHeader("accessToken");
-        log.info("token: {}", token);
+        log.info("새로운 로그인 요청자 token: {}", token);
 
         LoginResponse loginResponse = loginService.login(token);
-        log.info("로그인 성공: {}", token);
+        log.info("새로운 로그인 요청 수락. 로그인 성공: {}", token);
 
 //        Cookie cookie = new Cookie("accessToken", token);
 //        response.addCookie(cookie);
 
-        response.setHeader("Set-Cookie", "loginSession=" + token + "; HttpOnly");
+        response.setHeader("Set-Cookie", "loginSession=" + token + "; HttpOnly; SameSite=None");
         return loginResponse;
     }
 
     @PostMapping("/logout")
     @ResponseStatus(HttpStatus.OK)
     public void logout(HttpServletRequest request) {
+        log.info("로그아웃 요청 발생.");
         String token = CookieManager.findLoginSessionCookie(request);
         loginService.logout(token);
+        log.info("로그아웃 요청 수락.");
     }
 }
